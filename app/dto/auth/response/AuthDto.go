c7ce27bb1b02cf2f8506c36131ae48882a595580
yebology <139052533+yebology@users.noreply.github.com>
2025-10-23 14:37:06 +0700

    feat: done request and response dto for authentication

---
+++ app/dto/auth/response/AuthDto.go
@@ -0,0 +1,8 @@
+package response
+
+type AuthDto struct {
+	AccessToken		string 		`json:"accessToken"`
+	TokenType		string 		`json:"tokenType"`
+	ExpiresIn		int64 		`json:"expiresIn"`
+	User 			UserDto		`json:"user"`
+}
